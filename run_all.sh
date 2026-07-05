69059bf1a2c14342833ff374401b470d4076bc39
James Richer <richerjames@gmail.com>
2026-07-05 23:25:38 +0100

    fixed run_all

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export CUDA_VISIBLE_DEVICES=1,2
 python run_experiment.py -c configs/faust_shape_matching.yaml
 python run_experiment.py -c configs/scape_shape_matching.yaml
 python run_experiment.py -c configs/smal_shape_matching.yaml
