d10ecd6b8dcc1f51a081c07ed44df543b227562f
James Richer <richerjames@gmail.com>
2026-07-05 23:22:33 +0100

    added run all bash script

---
+++ run_all.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+python run_experiment.py -c configs/faust_shape_matching.yaml
+python run_experiment.py -c configs/scape_shape_matching.yaml
+python run_experiment.py -c configs/smal_shape_matching.yaml
